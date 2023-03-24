cdfa3a312c40507418a4251907eb7ca973df2bb6
James Hoctor <JEHoctor@protonmail.com>
2023-03-24 00:33:10 -0400

    Formatting

--- isosurface_vase/vase.py
+++ isosurface_vase/vase.py
@@ -142,7 +142,9 @@ def vase_scalar_field(points: npt.NDArray[np.float64]) -> npt.NDArray[np.float64
     """Calculate values in a scalar field whose isosurface at zero defines the wall of a vase."""
     xy, z = points[:, :2], points[:, 2]
     theta = np.arctan2(xy[:, 1], xy[:, 0])
-    return -np.linalg.norm(xy, axis=1) + (4.0 + 0.3 * np.sin(50 * theta + 15* np.sin(z) + 10 * z)) * ((z >= 1) & (z <= 9))
+    return -np.linalg.norm(xy, axis=1) + (4.0 + 0.3 * np.sin(50 * theta + 15 * np.sin(z) + 10 * z)) * (
+        (z >= 1) & (z <= 9)
+    )
 
 
 def _conf_axis(resolution: float, length: float, center: bool) -> Tuple[float, float, float]:
